3bbfaaed9029b2199b740fc114450ce39e35a12a
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-02-17 16:05:37 +0100

    hello route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use SebastianBergmann\Environment\Console;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +10,14 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class LoginController extends AbstractController
 {
+    #[Route('/api/hello', name: 'api_hello', methods: ['GET', 'POST'])]
+    public function hello(): Response
+    {
+      return ($this->json([
+        'content' => 'hello'
+      ]));
+    }
+
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function index(#[CurrentUser()] ?User $user): Response
       {
