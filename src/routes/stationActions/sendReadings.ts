691b1760fc0995e4e17548aa8b7e8edcdf2e1e58
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-02-19 13:49:18 +0100

    remove merge errors

--- src/routes/stationActions/sendReadings.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-let router = express.Router();
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.json({ message: "Données récupérées avec succès depuis l'URL" });
-});
-export default router;
--- src/routes/stationActions/stationRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import sendReading from "./sendReadings";
-
-// const express = require("express");
-let router = express.Router();
-
-router.use("/send-reading", sendReading);
-export default router;
