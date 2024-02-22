2f500acbc5164f7b79e5b92f4886432cc4fa86d4
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-02-22 15:14:24 +0100

    Room controller

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Building;
 use App\Entity\User;
+use App\Repository\BuildingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
-class BuildingController extends AbstractController
+class RoomController extends AbstractController
 {
-    #[Route('/api/building/list', name: 'building_list', methods: ['POST'])]
-    public function index(#[CurrentUser()] User $user, Request $request): Response
+    #[Route('/api/room/list', name: 'room_list', methods: ['POST'])]
+    public function index(#[CurrentUser()] User $user, Request $request, BuildingRepository $repo): Response
     {
         $session = $request->getSession();
         $token = $request->headers->get('token_user');
@@ -21,12 +23,13 @@ class BuildingController extends AbstractController
             'message' => 'missing credentials',
             ], Response::HTTP_UNAUTHORIZED);
         }
-
-        $buildings = $user->getBuildings();
+        $jsonbody = $request->getContent();
+        $body = json_decode($jsonbody, true);
+        $building = $repo->findOneBy(['id' => $body["building_id"]]);
+        $rooms = $building->getRooms();
         return $this->json([
             'message' => 'ok',
-            'list_building' => $buildings,
+            'list_room' => $rooms,
             ], Response::HTTP_OK);  
     }
-    
 }
