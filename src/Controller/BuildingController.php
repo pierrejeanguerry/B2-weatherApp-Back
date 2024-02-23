f37b978c3d1f7e9d76dbd82d6283d2d84f8e2c27
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-02-23 17:11:06 +0100

    create building

--- src/Controller/BuildingController.php
+++ src/Controller/BuildingController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Building;
 use App\Entity\User;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,4 +32,37 @@ class BuildingController extends AbstractController
             ], Response::HTTP_OK);  
     }
     
+    #[Route('/api/building/create', name: 'building_create', methods: ["POST"])]
+    public function create(#[CurrentUser()] User $user, Request $request, EntityManagerInterface $manager): Response
+    {
+        $session = $request->getSession();
+        $token = $request->headers->get('token_user');
+        if (null === $user || !($token == $session->get("token_user"))) {
+          return $this->json([
+            'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $manager->getConnection()->beginTransaction();
+
+        try{
+            $jsonbody = $request->getContent();
+            $body = json_decode($jsonbody, true);
+            $building = new Building;
+            $building->setName($body['name_building'])
+            ->setUser($user);
+            $manager->persist($building);
+            $manager->flush();
+            $manager->getConnection()->commit();
+            return $this->json([
+                'message' => 'building created',
+                ], Response::HTTP_CREATED);
+        } 
+        catch (UniqueConstraintViolationException $e){
+            $manager->getConnection()->rollBack();
+            return $this->json([
+                'message' => $e,
+                ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
