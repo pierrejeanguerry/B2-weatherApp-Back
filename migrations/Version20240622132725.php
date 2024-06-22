783a1573a84e435298275c2724c5f33e0d80a042
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-06-22 15:29:04 +0200

    resolve migrations

--- migrations/Version20240622132725.php
+++ migrations/Version20240622132725.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240222130820 extends AbstractMigration
+final class Version20240622132725 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,16 @@ final class Version20240222130820 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE building ADD user_id INT NOT NULL');
         $this->addSql('ALTER TABLE building ADD CONSTRAINT FK_E16F61D4A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_E16F61D4A76ED395 ON building (user_id)');
+        $this->addSql('ALTER TABLE reading ADD CONSTRAINT FK_C11AFC4121BDB235 FOREIGN KEY (station_id) REFERENCES station (id)');
+        $this->addSql('ALTER TABLE station ADD CONSTRAINT FK_9F39F8B14D2A7E12 FOREIGN KEY (building_id) REFERENCES building (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE building DROP FOREIGN KEY FK_E16F61D4A76ED395');
-        $this->addSql('DROP INDEX IDX_E16F61D4A76ED395 ON building');
-        $this->addSql('ALTER TABLE building DROP user_id');
+        $this->addSql('ALTER TABLE reading DROP FOREIGN KEY FK_C11AFC4121BDB235');
+        $this->addSql('ALTER TABLE station DROP FOREIGN KEY FK_9F39F8B14D2A7E12');
     }
 }
